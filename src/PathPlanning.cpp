1ee8dd2c9a9bfce77a688c4bfc50dc8263921f20
Parth Mehrotra <parth.mehrotra.cs@gmail.com>
2015-06-08 19:53:42 -0400

    added path-planning package

---
+++ src/PathPlanning.cpp
@@ -0,0 +1,9 @@
+#pragma once
+
+#include "PathPlanning.h"
+
+int PathPlanning::test() {
+    return 2;
+}
+
+int main(){ }
---
+++ src/PathPlanning.h
@@ -0,0 +1,6 @@
+#pragma once
+
+class PathPlanning {
+    public: 
+        int test();
+};
